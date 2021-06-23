745d52c9a3f35efab96e9c68e3c8a529a463696d
edpires <edpires4@sapo.pt>
2021-06-23 22:20:58 +0100

    Add Ajax

---
+++ ajax/scripts/servercalls.js
@@ -0,0 +1,14 @@
+﻿function lerDadosDoServidor(endereco,funcaodestino){
+    var ficheiro;
+    var resposta;
+
+    ficheiro=new XMLHttpRequest();
+    ficheiro.onreadystatechange=function(){
+        if(this.readyState==4 && this.status==200){
+            resposta=this.responseText;
+            funcaodestino(JSON.parse(resposta));
+        }
+    }
+    ficheiro.open("GET", endereco, true);
+    ficheiro.send();
+}
